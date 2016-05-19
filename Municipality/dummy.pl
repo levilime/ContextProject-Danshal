3a9c79042d251bf6a1b02cbd56480dbf39c1ca3a
Nick Cleintuar <N.A.Cleintuar@student.tudelft.nl>
2016-05-19 23:22:48 +0200

    Added rules for the new percepts implemented by Danshal

--- Municipality/dummy.pl
+++ Municipality/dummy.pl
@@ -3,6 +3,10 @@
 :- dynamic settings/1.
 :- dynamic functions/1.
 :- dynamic buildings/1.
+:- dynamic indicator/3.
+:- dynamic indicatorLink/2.
+:- dynamic stakeholder/4.
+:- dynamic debuggoal/0.
 
 % we have a building if the building list has at least 1 element.
 havebuilding :- buildings([X|Y]).
