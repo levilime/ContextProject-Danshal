80adfcd61ae2c35780fe88828fc82a932b9f5bc0
diony <diony@zeelandnet.nl>
2016-06-09 09:42:41 +0200

    implemented land5 in the azc module

--- Municipality/dummy.pl
+++ Municipality/dummy.pl
@@ -1,22 +1,22 @@
 %Definition for beliefs
-:- dynamic stakeholders/1,
-	settings/1,
+:- dynamic
+	actionlog/4,
+	building/7,
+	function/3,
 	functions/1,
-	self/1,
+	greenId/1,
+	havebuiltsomething/0,
 	indicator/4,
 	indicatorLink/2,
-	zone_link/4,
-	greenId/1,
-	stakeholder/4,
-	zone/5,
-	building/7,
-	land/3,
+	land/5,
+	my_stakeholder_id/1,
 	request/2,
-	havebuiltsomething/0,
-	land/3,
-	function/3,
-	actionlog/4,
-	my_stakeholder_id/1.
+	self/1,
+	settings/1,
+	stakeholder/4,
+	stakeholders/1,
+	zone/5,
+	zone_link/4.
 
 %We have a building if the building list has at least 1 element.
 havebuilding :- true.
@@ -27,4 +27,4 @@ money(StartBudget) :- stakeholder(_,'Gemeente',StartBudget,_).
 % We want a building if we need it
 buildPark :- building(BuildingID,_,OwnerID,_,Categories,_,_), member('PARK',Categories).
 % Azc's function ID is 650
-buildAZC(Land,Floors) :- building(BuildingID,_,OwnerID,_,_,650,Floors), land(Land,_,_).
+buildAZC(Land,Floors) :- building(BuildingID,_,OwnerID,_,_,650,Floors), land(Land,_,_,_,_).
